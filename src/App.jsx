f821ee5721c66a9c5273dad99de24c1fe6636b14
Shukra Tamang <shukratamang.npl@gmail.com>
2025-04-04 21:40:47 +1100

    setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Welcome</h1>
+      <h1 className="text-center font-bold">Welcome</h1>
     </>
   );
 }
