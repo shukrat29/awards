f1df57f64bbb9153a8dbf4eb429ef0965c1403d4
Shukra Tamang <shukratamang.npl@gmail.com>
2025-04-05 19:49:34 +1100

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center font-bold">Welcome</h1>
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
   );
 }
 
